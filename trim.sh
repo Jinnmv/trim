b2ded145dee0b512b61016ec5aaa32c1e9d25101
Anton Savenko <anton.a.savenko@gmail.com>
2014-06-17 20:05:57 +0300

    log file name moved to variable

--- trim.sh
+++ trim.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Script to run fstrim on root and log results to /var/log/trim.log in user-friendly manere
 
+logFile="/var/log/trim.log"
+
 trimSize=$( fstrim / -v | cut -f 2 -d ' ')
 trimSize=$((trimSize / (1024 * 1024)))
 
 echo "fstrim: $trimSize Mb were trimmed"
-echo -e "$(date +'%d %b %Y %T'):\t$trimSize Mb were trimmed" >> /var/log/trim.log
+echo -e "$(date +'%_d %b %Y %T'):\t$trimSize Mb were trimmed" >> $logFile 
 
